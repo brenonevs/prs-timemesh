f9985f3e7c9d2d39c33c91fb1943c6cac7e6512a
brenonevs <brenobraganevs@gmail.com>
2025-05-02 20:27:03 -0300

    [feat] Implemented user registration endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import UserRegistrationSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "message": "Usuário registrado com sucesso!",
+                "user": {
+                    "username": user.username,
+                    "email": user.email,
+                    "first_name": user.first_name,
+                    "last_name": user.last_name
+                }
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
