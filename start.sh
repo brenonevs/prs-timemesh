43ad84194cb4d15b6514275d2dfcff4d8a96bec9
brenonevs <brenobraganevs@gmail.com>
2025-05-02 19:51:49 -0300

    [sec] Fixing basic security errors

--- start.sh
+++ start.sh
@@ -16,9 +16,9 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
 import os
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@admin.com')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
 " && echo "Superusuário criado ou já existe."
